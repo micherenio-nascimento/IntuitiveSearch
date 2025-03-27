af06e81415ab7de54eab7b71073e12b9a0479420
micherenio <micherenio.nascimento@gmail.com>
2025-03-27 07:23:58 -0300

    Refatoração do endpoint para buscar todos ou por filtros

--- Backend/api/operator_routes.py
+++ Backend/api/operator_routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from services.operator_service import OperatorService
 from repositories.operator_repository import OperatorRepository
+from typing import Optional
 
 router = APIRouter()
 
@@ -8,7 +9,11 @@ repository = OperatorRepository("data/Relatorio_cadop_ficticio.csv")
 service = OperatorService(repository)
 
 @router.get("/operators/search")
-async def search_operators(query: str):
-    """Endpoint para buscar operadores"""
-    results = service.search_operators(query)
+async def search_operators(query: Optional[str] = None, 
+                            razao_social: Optional[str] = None, 
+                            nome_fantasia: Optional[str] = None, 
+                            cnpj: Optional[str] = None, 
+                            registro_ans: Optional[str] = None):
+    """Endpoint para buscar operadores com filtros"""
+    results = service.search_operators(query, razao_social, nome_fantasia, cnpj, registro_ans)
     return {"operators": [op.dict() for op in results]}
