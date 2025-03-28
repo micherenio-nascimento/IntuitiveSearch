a4d12f2b1a0b2e9c5b8c3e97225016fcb7d83f61
micherenio <micherenio.nascimento@gmail.com>
2025-03-28 20:13:43 -0300

    Criação do componente app

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Busca de Operadoras</h1>
+    
+    <search-bar
+      v-model:query="viewModel.state.searchQuery"
+      @search="viewModel.search"
+    />
+    
+    <div v-if="viewModel.state.loading" class="loading">Carregando...</div>
+    
+    <operator-list
+      v-if="viewModel.state.operators.length"
+      :operators="viewModel.state.operators"
+    />
+    
+    <div v-if="viewModel.state.error" class="error">{{ viewModel.state.error }}</div>
+    <div v-if="!viewModel.state.loading && !viewModel.state.operators.length && viewModel.state.searchQuery" class="no-results">
+      Nenhum resultado encontrado
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from './components/SearchBar.vue';
+import OperatorList from './components/OperatorList.vue';
+import createOperatorViewModel from './viewmodels/OperatorViewModel';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SearchBar,
+    OperatorList
+  },
+  setup() {
+    const viewModel = createOperatorViewModel();
+    viewModel.loadInitialData();
+    return { viewModel };
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-bottom: 20px;
+}
+
+.loading {
+  text-align: center;
+  padding: 20px;
+}
+
+.error {
+  color: red;
+  text-align: center;
+  padding: 20px;
+}
+
+.no-results {
+  text-align: center;
+  padding: 20px;
+  color: #666;
 }
 </style>
